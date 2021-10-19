f3b0e525c2ff2c29b4345aec2b14a01c8e365130
numanayhan <numanayhan@yandex.com>
2021-10-19 11:50:16 +0300

    App Distribution eklendi.

--- Github iOS/Github iOS/Launch/Launch.swift
+++ Github iOS/Github iOS/Launch/Launch.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import TinyConstraints
+import Firebase
 
 class Launch: UIViewController {
     lazy var logo : TitleLabel = {
@@ -45,4 +46,34 @@ class Launch: UIViewController {
     deinit {
         stopTimer()
     }
+    override func viewDidAppear(_ animated: Bool) {
+       checkForUpdate()
+    }
+    private func checkForUpdate() {
+        AppDistribution.appDistribution().checkForUpdate(completion: { [self] release, error in
+          var uiAlert: UIAlertController
+
+          if error != nil {
+            uiAlert = UIAlertController(title: "Error", message: "Error Checking for update! \(error?.localizedDescription ?? "")", preferredStyle: .alert)
+          } else if release == nil {
+            uiAlert = UIAlertController(title: "Check for Update", message: "No releases found!!", preferredStyle: .alert)
+            uiAlert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default))
+          } else {
+            guard let release = release else { return }
+
+            let title = "New Version Available"
+            let message = "Version \(release.displayVersion)(\(release.buildVersion)) is available."
+            uiAlert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+            uiAlert.addAction(UIAlertAction(title: "Update", style: UIAlertAction.Style.default) {
+              _ in
+              UIApplication.shared.open(release.downloadURL)
+            })
+            uiAlert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel) {
+              _ in
+            })
+          }
+          self.present(uiAlert, animated: true, completion: nil)
+        })
+      }
 }
